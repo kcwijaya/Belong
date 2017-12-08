a43421d1c233e51b115d37f1a5e153e3dcf6ff35
Tristan Vanech <tristanvanech@DN0a1fc2f4.SUNet>
2017-12-08 06:24:08 -0800

    added settings and logout button

--- BelongProject/screens/Settings/SettingsHome.js
+++ BelongProject/screens/Settings/SettingsHome.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  TouchableHighlight,
   Text,
   View
 } from 'react-native';
-import TabNavigator from 'react-native-tab-navigator';
+import TabNavigator from 'react-native-tab-navigator'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import {Dimensions} from 'react-native'
+import { Colors } from '../../constants/Colors.js'
+import ProfilePicture from '../../components/people/ProfilePicture.js'
+import PeerSummary from '../../components/people/PeerSummary.js'
+import {Header, Body, Left, Right, Title, Button} from "native-base"
+import { Switch } from 'react-native-switch'
+import LargeButton from '../../components/buttons/LargeButton.js'
 
 const deviceW = Dimensions.get('window').width
 
@@ -18,12 +25,95 @@ function px2dp(px) {
 }
 
 export default class Settings extends Component {
+
+  static navigationOptions = ({ navigation }) => ({
+    header: null
+  });
+
   render() {
     return (
+
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Settings
-        </Text>
+        <Header>
+          <Title style={{fontWeight: '300'}} color={Colors.accent}>Settings</Title>
+        </Header>
+
+        <Header>
+          <Left>
+            <Text color={Colors.accent}>Info</Text>
+          </Left>
+          <Right>
+            <Text color={Colors.accent}>Public?</Text>
+          </Right>
+        </Header>
+
+        <View style={styles.info}>
+          <Left>
+            <Text>Name: Tristan Vanech</Text>
+          </Left>
+          <Right>
+            <Switch 
+              value={false}
+              onValueChange={(val) => console.log(val)}
+              backgroundActive={Colors.accent}/>
+          </Right>
+        </View>
+
+        <View style={styles.info}>
+          <Left>
+            <Text>School: Palo Alto High School</Text>
+          </Left>
+          <Right>
+            <Switch 
+              value={false}
+              onValueChange={(val) => console.log(val)}
+              backgroundActive={Colors.accent}/>
+          </Right>
+        </View>
+
+        <View style={styles.info}>
+          <Left>
+            <Text>Year: Junior</Text>
+          </Left>
+          <Right>
+            <Switch 
+              value={false}
+              onValueChange={(val) => console.log(val)}
+              backgroundActive={Colors.accent}/>
+          </Right>
+        </View>
+
+        <View style={styles.info}>
+          <Left>
+            <Text>Gender: Male</Text>
+          </Left>
+          <Right>
+            <Switch 
+              value={false}
+              onValueChange={(val) => console.log(val)}
+              backgroundActive={Colors.accent}/>
+          </Right>
+        </View>
+
+        <View style={styles.info}>
+          <Left>
+            <Text>Last Active: Now</Text>
+          </Left>
+          <Right>
+            <Switch 
+              value={false}
+              onValueChange={(val) => console.log(val)}
+              backgroundActive={Colors.accent}/>
+          </Right>
+        </View>
+
+        <View style={styles.buttonGroup}>
+          <TouchableHighlight style={styles.logout}>
+              <Text style={styles.buttonText}> Logout </Text>
+              </TouchableHighlight>
+              <View style={{width: 10}} />
+        </View>
+
       </View>
     )
   }
@@ -32,18 +122,34 @@ export default class Settings extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  info: {
+    flex: 1,
+    flexDirection: 'row',
   },
-  instructions: {
+  buttonText: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    color: 'white',
+    fontWeight: '300'
+  },
+  logout: {
+    backgroundColor: Colors.buttongen,
+    borderRadius:10,
+    borderWidth: 2,
+    borderColor: 'transparent',
+    width: 140,
+    height: 40, 
+    paddingBottom: 5,
+    paddingTop: 6,
+    overflow: 'hidden'
+  },
+  buttonGroup: {
+    flexDirection: 'row', 
+    flex: 1,
+    justifyContent: 'center',
+    marginTop: 20
   },
 });
